28b218e33e68433ebc1f49490ad27d31c498db48
fcha <francisco@monger.com>
2016-08-09 15:35:37 -0700

    Adjust schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,12 +20,19 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
+     *  ┌───────────── min (0 - 59)
+     *  │ ┌────────────── hour (0 - 23)
+     *  │ │ ┌─────────────── day of month (1 - 31)
+     *  │ │ │ ┌──────────────── month (1 - 12)
+     *  │ │ │ │ ┌───────────────── day of week (0 - 6) (0 to 6 are Sunday to Saturday, or use names; 7 is also Sunday)
+     *  │ │ │ │ │ ┌────────────────── year (1900 - 3000)
+     *  * * * * * *  command to execute
+     *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('coc:fetchClan')
-                 ->everyTenMinutes();
+                 ->cron('0,1,2,3,4,5,55,56,57,58,59 0,1,23 * * * *');
     }
 }
