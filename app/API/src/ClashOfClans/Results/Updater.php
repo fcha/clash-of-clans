f5958017e7720fecc5637e2f96d1d63f433061f7
fcha <francisco@monger.com>
2016-08-09 20:42:13 -0700

    Create updater service

---
+++ app/API/src/ClashOfClans/Results/Updater.php
@@ -0,0 +1,35 @@
+<?php namespace App\API\src\ClashOfClans\Results;
+
+use App\API\src\ClashOfClans\Results\Storage\Repositories\Eloquent\RepositoryInterface as Repository;
+
+class Updater {
+
+	/**
+	 * @var  \App\API\src\ClashOfClans\Results\Storage\Repositories\Eloquent\RepositoryInterface
+	 */
+	protected $repository;
+
+	/**
+	 * @param  \App\API\src\ClashOfClans\Results\Storage\Repositories\Eloquent\RepositoryInterface    $repository
+	 */
+	public function __construct(Repository $repository)
+	{
+		$this->repository = $repository;
+	}
+
+	/**
+	 * Gets the clan  information
+	 *
+	 * @return array
+	 */
+	public function completeResults($results)
+	{
+		$completedStatusId = config('api.results.statuses.completed');
+
+		foreach ($results as $id)
+		{
+			$this->repository->setStatus($id, $completedStatusId);
+		}
+	}
+
+}
