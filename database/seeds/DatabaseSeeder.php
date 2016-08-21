4ecb7f11f652c0a555f8ff4c07848326959779a8
fcha <franciscocha@gmail.com>
2016-08-21 16:11:12 -0700

    Create new seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call(memberDetailStatuses::class);
         $this->call(resultTypes::class);
         $this->call(resultStatuses::class);
         $this->call(roles::class);
